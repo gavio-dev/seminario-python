bb9acf86ecdf0e650c989b69b892241d9ab56788
Gaston Gavio <ggavioctc@gmail.com>
2026-03-18 22:56:55 -0300

    Agrega ejercicio 1 terminado

---
+++ ejercicio1.py
@@ -0,0 +1,7 @@
+'''1. Escribe un programa que solicite al usuario su año de nacimiento y muestre en qué
+año cumplirá 18, 21 y 100 años.'''
+
+ano=int(input('Ingrese su año de nacimiento'))
+print('Usted cumplira 18 en el año',ano+18,)
+print('Usted cumplira 21 en el año',ano+21,)
+print('Usted cumplira 100 en el año',ano+100,)
